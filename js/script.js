6d71e669c2912635ae99ec44450b4bc84cf68eb5
Ilya <everjupe13@gmail.com>
2019-11-22 14:24:25 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -26,33 +26,104 @@ const slider1 = tns({
     // }
 });
 
-function stopDefAction(evt) {
-    evt.preventDefault();
-}
+
+const slider2 = tns({
+    container: '.license__wrapper-carousel',
+    items: 1,
+    slideBy: 1,
+    nav: false,
+    navPosition: 'top',
+    autoplay: false,
+    controls: false,
+});
+
+
+document.querySelector('.prev').onclick = function () {
+    slider2.goTo('prev');
+};
+
+document.querySelector('.next').onclick = function () {
+    slider2.goTo('next');
+};
+
+
+
+// function stopDefAction(evt) {
+//     evt.preventDefault();
+// }
     
-document.getElementById('calculator__form').addEventListener(
-    'click', stopDefAction, false
-);
+// document.getElementById('calculator__form').addEventListener(
+//     'click', stopDefAction, false
+// );
 
 
 $(document).ready(function() {
 
+    function validateFormsMain(form) {
+        $(form).validate({
+            rules: {
+                fromCity: "required",
+                fromStreet: "required",
+                fromHouse: "required",
+                whereCity: "required",
+                whereStreet: "required",
+                whereHouse: "required",
+                phone: "required",
+                fromCityMobile: "required",
+                whereCityMobile: "required",
+                fromStreetMobile: "required",
+                whereStreetMobile:"required",
+            },
+            messages: {
+                fromCity: "Пожалуйста, заполните поле",
+                fromStreet: "Пожалуйста, заполните поле",
+                fromHouse: "Пожалуйста, заполните поле",
+                whereCity: "Пожалуйста, заполните поле",
+                whereStreet: "Пожалуйста, заполните поле",
+                whereHouse: "Пожалуйста, заполните поле",
+                fromCityMobile: "Пожалуйста, заполните поле",
+                whereCityMobile: "Пожалуйста, заполните поле",
+                fromStreetMobile: "Пожалуйста, заполните поле",
+                whereStreetMobile:"Пожалуйста, заполните поле",
+                phone: "Пожалуйста, введите свой номер телефона",
+            }
+        });
+    }
+
     function validateForms(form) {
         $(form).validate({
             rules: {
-                name: "required",
                 phone: "required",
             },
             messages: {
-                name: "Пожалуйста, введите свое имя",
                 phone: "Пожалуйста, введите свой номер телефона",
             }
         });
     }
 
     validateForms('#promo__form');
-    validateForms('#calculator__form');
+    validateFormsMain('#calculator__form');
 
     $('input[name=phone]').mask("+7 (999)-999-9999");
+
+
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('.overlay, #thanks').fadeIn('slow')
+            $('form').trigger('reset');
+        });
+        return false;
+    })
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #thanks').fadeOut('slow');
+    });
 
 })
