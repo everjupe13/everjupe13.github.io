c34791d7b26260d2dc7dfdbea27c38a16000de68
Ilya <everjupe13@gmail.com>
2020-04-06 20:02:25 +0300

    Add features for widget

--- widget/js/widget.js
+++ widget/js/widget.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+    window.onload = function() {
+        setTimeout(autoWidget, 5500);
+    };
+
+    function autoWidget() {
+        $('#lbw-close').focus().click();
+    }
+
     $('#lbw-close').on('click', function() {
         $('#lbw-body').toggleClass('lbw-body-active');
 
@@ -23,15 +31,27 @@ $(document).ready(function() {
         $(someId).fadeIn('slow');
     }
 
+    function showMyText(text, placeId, inputId) {
+        $(placeId).css('display', 'block');
+        $(placeId).text(text);
+        $(inputId).val();
+    }
+
+    $('#lbw-asked-input').keypress(function(e) {
+        if(e.which == 13) {
+            $(this).blur();
+            $('#lbw-asked-btn').focus().click();
+        }
+    });
 
     $('#lbw-asked-btn').on('click', function(){
         if(($('#lbw-bot2').css('display') == 'block')&&(!$('#lbw-asked-btn').hasClass('stopped'))) {
+            $('#lbw-asked-btn').addClass('stopped');
             var userText = $('#lbw-asked-input').val();
             $('#lbw-user1').css('display', 'block');
             $('#lbw-user1').text(userText);
             $('#lbw-asked-input').val("");
-            $('#lbw-asked-btn').addClass('stopped');
-            setTimeout(finalAct, 2000, "#qu-form");
+            setTimeout(finalAct, 2500, "#qu-form");
         }
     });
 
